7bc55b2fa6ae1a06753893f76bd43d97e15792ef
lilylyw <630234755@qq.com>
2018-08-02 14:59:56 +0800

    Create checkbox.js

---
+++ 01/16JS08/js/checkbox.js
@@ -0,0 +1,11 @@
+/*Checkbox Module*/
+/*Render checkbox*/
+//value,text,both Array type
+function createCheckboxes(container,value,text) {
+	let str1 = '<input type='checkbox' data-text='全选'><span>全选</span>';
+	let str2 = '';
+	for(let i = 0;i < 3;i++) {
+		str2 +='<input type='checkbox' id='${value[i]}' value='${value[i]}' data-text='${text[i]}'><span>${text[i]}</span>';
+	}
+	container.innerHTML = str1 + str2;
+}
